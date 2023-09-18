76fb26965e082b3f5d13551824b61e104c07b030
Elijah <elijahd.camacho@gmail.com>
2023-09-18 19:55:20 -0400

    File added

---
+++ let and const Exercise.js
@@ -0,0 +1,21 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+/* Write an ES2015 Version */
+const PI = 3.14;
+
+
+
+//Quiz
+
+//What is the Difference between var and let?
+//Var can be redeclared but let cannot.
+
+//What is the difference between var and const?
+//Const cannot be redeclared or reassigned.
+
+//What is the difference between let and const?
+//Const cannot be reassigned.
+
+//What is hoisting?
+//Variables will be run first, put at the top of the scope it is in. Functions also work this way and can be called before they are defined.
